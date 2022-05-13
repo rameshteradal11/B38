8c0483da2e284689c80b06228e409df153e64711
EMPTY <EMPTY@S>
2022-05-13 08:01:37 +0530

    Code push-Test cases

--- src/test/java/generic/BaseTest.java
+++ src/test/java/generic/BaseTest.java
@@ -1,11 +1,15 @@
 package generic;
 
+import java.io.File;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Iterator;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -13,6 +17,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -23,6 +28,7 @@ import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -50,7 +56,6 @@ public class BaseTest implements IAutoConst {
 	@BeforeMethod
 	public void createTest(Method testMethod) {
 		String testName=testMethod.getName();
-		System.out.println("***"+testName+"***");
 		test = report.createTest(testName);
 	}
 	
@@ -59,6 +64,7 @@ public class BaseTest implements IAutoConst {
 	public void openApp(@Optional(CONFIG_PATH) String config) throws MalformedURLException {
 
 		String browser=Utility.getProperty(config,"BROWSER");
+		test.info("Browser:"+browser);
 		String grid=Utility.getProperty(config, "GRID");
 		if(grid.equalsIgnoreCase("Yes"))
 		{
@@ -103,7 +109,26 @@ public class BaseTest implements IAutoConst {
 	}
 	
 	@AfterMethod(alwaysRun = true)
-	public void closeApp() {
+	public void closeApp(ITestResult result) throws Exception {
+		
+		int status=result.getStatus();
+		String testName=result.getName();
+		if(status==2) 
+		{
+			TakesScreenshot t=(TakesScreenshot)driver;
+			File srcfile = t.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(srcfile, new File(IMG_PATH+testName+".png")); //add test name with date and time
+			//add the screenshot to the test -at the top
+//			test.addScreenCaptureFromPath("./../img/test.png");
+			
+			//add the screenshot to the test - inside the step
+			test.fail(MediaEntityBuilder.createScreenCaptureFromPath("./../img/"+testName+".png").build());
+			
+			String msg=result.getThrowable().getMessage();
+			test.fail("Reason:"+msg);  //add screenshot of the application to extent report
+			
+		}
+		
 		driver.quit();
 	}
 	
